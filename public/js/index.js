520a6174164c8b952a07aae832eea9e64971db22
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2023-12-15 16:35:38 -0300

    adicionando condicao ao enviar um campo de formulario vazio, usando jquery

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#meuFormulario').on('submit', function(event){
+        const nome = $('#exampleInputName1').val();
+        const email = $('#exampleInputEmail1').val();
+        const password = $('#exampleInputPassword1').val();
+
+        if(nome === '' || email === '' || password === ''){
+            alert('Por favor, preencha todos os campos.');
+            event.preventDefault();
+        }
+    })
+
+});
