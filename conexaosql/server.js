375441ec15b3f352192b188f4f8995813f27993f
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2023-12-18 15:06:29 -0300

    criado metodo created e funcao de checagem de Email

---
+++ conexaosql/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
+
+const checkEmail = require('../checagem/checkEmail.js');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '..', '..', 'form_db', 'public')));
+
+const routes = require(path.join(__dirname, '..','routes', 'routes.js'));
+
+app.use('/', routes);
+
+
+app.listen(port, () => {
+    console.log(`Servidor OK em http://localhost:${port}`);
+})
